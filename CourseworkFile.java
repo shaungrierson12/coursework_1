1bb09ddd7e34f296141ff2bbda9aba17dbfc39b8
Shaun Grierson <Shaun.grierson@city-holdings.co.uk>
2019-11-08 21:20:33 +0000

    add guard clause

--- CourseworkFile.java
+++ CourseworkFile.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
-class Dec2Hex
+class main
 {
 
     public static int Arg1;
 
     public static void main(String args[])
     {
-        if (args.length > 0) 
+        if (args == null) 
         {
+     		System.err.println("No Arguments Provided");
+                System.exit(1);
+	}
             try 
             {
                 Arg1 = Integer.parseInt(args[0]);
@@ -18,7 +21,7 @@ class Dec2Hex
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
             }
-        }
+        
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
